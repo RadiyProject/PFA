e4cbcb5d53f8bf78e0e1c5da35e16090fcda6a7f
RadiyProject <sigor00@mail.ru>
2022-03-25 15:19:17 +0700

    Initial state

---
+++ PFA/PFA/PFA/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using PFA.ViewModels;
+using PFA.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PFA
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ PFA/PFA/PFA/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PFA.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PFA.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
