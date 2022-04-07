e2b4829b5b01a3d2366767cbdc98ef5a1545ba84
Mr-kopeika <102347082+Mr-kopeika@users.noreply.github.com>
2022-04-07 22:14:38 +0700

    07.04.2022-Version-1(Konstantitya)

---
+++ Good-Checkue/Good.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    class Good
+    {
+        public string name;
+        public float price;
+
+        public Good(string name, float price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+
+    }
+
