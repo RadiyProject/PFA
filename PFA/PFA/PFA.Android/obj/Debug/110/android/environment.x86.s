439b620dbfe0ed5d97910dcf3e224f41e14b2578
OlgaBaginskaya <olga.baginskaya2@gmail.com>
2022-04-21 20:30:54 +0700

    InfoBlockCommit

--- PFA/PFA/PFA.Android/obj/Debug/110/android/environment.x86.s
+++ PFA/PFA/PFA.Android/obj/Debug/110/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"e9426fef-3dee-44f4-8094-d86702074ae5"
+	.asciz	"14824ef4-2d6c-41d9-b072-78c2e9a082f0"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
