054087c17cfd2696fb57ec9f885ae2fa7f40e4c0
Kupmu <87134942+Kupmu@users.noreply.github.com>
2022-05-04 21:15:28 +0700

    bd_user

---
+++ WCFLibraryForTP/WCFLibraryForTP/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFLibraryForTP
+{
+    class User
+    {
+        private string userId { get; set; }
+        private string userPassword { get; set; }
+
+        public User() { }
+
+        public User(string userPassword)
+        {
+            this.userPassword = userPassword;
+        }
+    }
+}
