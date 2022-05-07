485d1344e2c9b4fea45d9dbe82e2b1aacfbd2401
Mr-kopeika <102347082+Mr-kopeika@users.noreply.github.com>
2022-05-07 12:43:46 +0700

    Update API

--- WCFLibraryForTP/WCFLibraryForTP/IService1.cs
+++ WCFLibraryForTP/WCFLibraryForTP/IService1.cs
@@ -13,15 +13,43 @@ namespace WCFLibraryForTP
     public interface IService1
     {
         [OperationContract]
-        string AddUser(string login, string password);
+        string AddUser(string login, string password); //complete
         [OperationContract]
-        int CheckLogin(string login);
+        int CheckLogin(string login);//complete
         [OperationContract]
-        int SignIn(string login, string password);
+        int SignIn(string login, string password);//complete
         [OperationContract]
-        int AddUserObject(User user);
+        int AddUserObject(User user);//complete
         [OperationContract]
-        int SignInObject(User user);
+        int SignInObject(User user);//complete
+        [OperationContract]
+
+        string AddBudget(string userId); //return result: 1 - success, 2 - unsuccess
+        [OperationContract]
+        Budget GetBudget(string userId); //return Budget of this User
+        [OperationContract]
+        string UpdateBudget(string userId, Budget budget); //return result: 1 - success, 2 - unsuccess
+
+        [OperationContract]
+        List<Category> GetCategories(); //return list of categories
+
+        [OperationContract]
+        int AddCheque(string userId, Cheque cheque); //DO NOT IMPLEMENT, return chequeId
+        [OperationContract]
+        string DeleteCheque(string userId, int idCheque);//DO NOT IMPLEMENT
+        [OperationContract]
+        Cheque GetCheque(string userId, int idCheque);//DO NOT IMPLEMENT
+        [OperationContract]
+        string UpdateCheque(string userId, int idCheque);//DO NOT IMPLEMENT
+
+        [OperationContract]
+        int AddGood(string userId, Good good); //DO NOT IMPLEMENT, return good id
+        [OperationContract]
+        string DeleteGood(string userId, int idGood);//DO NOT IMPLEMENT
+        [OperationContract]
+        Good GetGood(string userId, int idGood);//DO NOT IMPLEMENT
+        [OperationContract]
+        string UpdateGood(string userId, int idGood);//DO NOT IMPLEMENT
     }
 
 
